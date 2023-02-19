2556dead553e95c1fe9c935e6b86304609166f3d
Crpearce <Crpearce892@gmail.com>
2023-02-18 22:07:08 -0700

    add nav and footer files

--- src/App.js
+++ src/App.js
@@ -1,44 +1,57 @@
-import {useEffect, useState} from 'react';
-import './App.css';
+import { useEffect, useState } from "react";
+import Navbar from "./components/navbar/navbar.component";
+import Footer from "./components/footer/footer.component";
+import "./App.css";
 
 function App() {
-  const CLIENT_ID = "a959c9fd78fb4067ab8a08680b4fd97e"
-  const REDIRECT_URI = "http://localhost:3000"
-  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
-  const RESPONSE_TYPE = "token"
+  const CLIENT_ID = "a959c9fd78fb4067ab8a08680b4fd97e";
+  const REDIRECT_URI = "http://localhost:3000";
+  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
+  const RESPONSE_TYPE = "token";
 
-  const [token, setToken] = useState("")
+  const [token, setToken] = useState("");
 
   useEffect(() => {
-      const hash = window.location.hash
-      let token = window.localStorage.getItem("token")
+    const hash = window.location.hash;
+    let token = window.localStorage.getItem("token");
 
-      if (!token && hash) {
-          token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
+    if (!token && hash) {
+      token = hash
+        .substring(1)
+        .split("&")
+        .find((elem) => elem.startsWith("access_token"))
+        .split("=")[1];
 
-          window.location.hash = ""
-          window.localStorage.setItem("token", token)
-      }
+      window.location.hash = "";
+      console.log(token);
+      window.localStorage.setItem("token", token);
+    }
 
-      setToken(token)
-
-  }, [])
+    setToken(token);
+  }, []);
 
   const logout = () => {
-      setToken("")
-      window.localStorage.removeItem("token")
-  }
+    setToken("");
+    window.localStorage.removeItem("token");
+  };
 
   return (
-      <div className="App">
-          <header className="App-header">
-              <h1>Spotify React</h1>
-              {!token ?
-                  <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login
-                      to Spotify</a>
-                  : <button onClick={logout}>Logout</button>}
-          </header>
-      </div>
+    <div className="App">
+      <Navbar />
+      <header className="App-header">
+        <h1>Spotify React</h1>
+        {!token ? (
+          <a
+            href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}
+          >
+            Login to Spotify
+          </a>
+        ) : (
+          <button onClick={logout}>Logout</button>
+        )}
+      </header>
+      <Footer />
+    </div>
   );
 }
 
---
+++ src/components/navbar/navbar.component.jsx
@@ -0,0 +1,11 @@
+import "./navbar.styles.css";
+
+const Navbar = () => {
+    return (
+        <div>
+            Navbar
+        </div>
+    );
+}
+
+export default Navbar;
